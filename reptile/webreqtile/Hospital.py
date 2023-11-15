0c329228f25fd65e9a29042c462b1ecb6ffde35e
hhuabin <hhuabin@qq.com>
2023-11-15 23:27:22 +0800

    commit

--- reptile/webreqtile/Hospital.py
+++ reptile/webreqtile/Hospital.py
@@ -10,21 +10,40 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 # 开始抢购时间，建议提前一两秒开始抢购
-purchaseTime = "2023-11-16 10:07:58"
-# 华为商城购买页网址。如 mate60pro 购买网址
-url = "https://patientcloud.sysucc.org.cn/webappm/mainModule/Website"
+purchaseTime = "2023-11-16 16:00:00"
+# 中山大学附属肿瘤医院网址
+url = "https://patientcloud.sysucc.org.cn/webappm/mainModule/homePage"
 
 # path = "chromedriver.exe"
 browser = webdriver.Chrome()
 # 打开网址
 browser.get(url)
 
+# 选日期刷新按钮
+dateButtonXpath = ("//div[@id='app']/div[@class='pageBase']/div[@class='middleView']"
+                   "/div[@class='date-wrap rCenterStart']/div[3]")
+# 选号按钮
+buyButtonXpath = ("//div[@id='app']/div[@class='pageBase']/div[@class='middleView']/div[@class='point-list']"
+                  "//div[@class='point-item rCenterStart'][1]//div[@class='point-state have-point-state fz-sm']")
+# 确定按钮
+confirmButtonXpath = "//body/div[@id='app']/div[@class='point-confirm']/div[@class='point-info']//button"
 
 startTime = datetime.strptime(purchaseTime, "%Y-%m-%d %H:%M:%S").timestamp()
 nowTime = datetime.now().timestamp()
 
 while startTime - nowTime >= 0:
-    print("准备抢购...", datetime.now())
+    print("准备开始...", datetime.now())
     time.sleep(0.1)
     nowTime = datetime.now().timestamp()
+
+# 开始
+purchaseStartTime = datetime.now()
+
+dateButton = browser.find_element(By.XPATH, dateButtonXpath)
+dateButton.click()
+print("选择日期成功")
+
+print("选号成功")
+
+print("确认订单，待支付...")
 
